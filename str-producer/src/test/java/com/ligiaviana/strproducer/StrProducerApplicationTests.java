5e383bd8aa32ee56c9b137ac143753298a6fb1fe
Ligia Viana <ligia.viana.azevedo@gmail.com>
2024-05-16 15:08:47 -0300

    str-producer started

---
+++ str-producer/src/test/java/com/ligiaviana/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ligiaviana.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
