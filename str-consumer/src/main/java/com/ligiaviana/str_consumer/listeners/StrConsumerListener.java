75a58eb07a1aa75ef81e16124670dd1681cacc44
Ligia Viana <ligia.viana.azevedo@gmail.com>
2024-05-28 11:52:38 -0300

    Exception - Error Handler

--- str-consumer/src/main/java/com/ligiaviana/str_consumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/ligiaviana/str_consumer/listeners/StrConsumerListener.java
@@ -1,6 +1,7 @@
 package com.ligiaviana.str_consumer.listeners;
 
 import com.ligiaviana.str_consumer.custom.StrConsumerCustomListener;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -10,9 +11,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class StrConsumerListener {
 
+    @SneakyThrows
     @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message) {
         log.info("CREATE ::: Receive message {}", message);
+        throw new IllegalArgumentException("EXCEPTION...");
     }
 
     @StrConsumerCustomListener(groupId = "group-1")
